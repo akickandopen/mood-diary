263ff94c900cf72efaccf753ce1a7d24d88c9f42
akickandopen <akilah.lapure@gmail.com>
2022-06-03 11:13:30 +0400

    Add comments

--- js/save-result.js
+++ js/save-result.js
@@ -1,16 +1,21 @@
-let txtTitle = document.getElementById("title");
-let txtBody = document.getElementById("body");
-let txtMood = document.getElementById("mood");
-let txtDate = document.getElementById("date");
+// Set variables for elements from html
+const txtTitle = document.getElementById("title");
+const txtBody = document.getElementById("body");
+const txtMood = document.getElementById("mood");
+const txtDate = document.getElementById("date");
 
+// Get values from local storage
 const title = localStorage.getItem("title");
 const body = localStorage.getItem("body");
 const mood = localStorage.getItem("mood");
+
+// Get today's date
 const date = new Date();
 date.getFullYear();
 date.getMonth();
 date.getDay();
 
+// Set values in html elements to values from local storage
 txtTitle.innerHTML = title;
 txtBody.innerHTML = body;
 txtMood.innerHTML = "Feeling: " + mood;
@@ -21,6 +26,8 @@ console.log(body);
 console.log(mood);
 
 async function createImage(){
+
+    // Define HTML and CSS with JSON object
     const json = {
         html: `
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -56,19 +63,20 @@ async function createImage(){
         google_fonts: "Poppins"
     };
 
-    const username = ""; // Your user id
-    const password = ""; // Your api key
+    const user_id = ""; // Your user id
+    const api_key = ""; // Your api key
 
     const options = {
         method: 'POST',
         body: JSON.stringify(json),
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': 'Basic ' + btoa(username + ":" + password)
+            'Authorization': 'Basic ' + btoa(user_id + ":" + api_key)
         }
     }
 
-    fetch('https://hcti.io/v1/image', options)
+    // Create a POST request to the API to create the image by using the fetch API
+    fetch('https://hcti.io/v1/image', options) 
         .then(res => {
             if (res.ok) {
                 return res.json();
@@ -79,20 +87,20 @@ async function createImage(){
         .then(data => {
             // Image URL is available here
             console.log(data.url);
-            downloadImage(data.url);
+            downloadImage(data.url); // Pass image url to function
         })
         .catch(err => console.error(err));
 }
 
 async function downloadImage(imageSrc) {
-    const image = await fetch(imageSrc)
-    const imageBlog = await image.blob()
-    const imageURL = URL.createObjectURL(imageBlog)
+    const image = await fetch(imageSrc); // fetch image url
+    const imageBlob = await image.blob();
+    const imageURL = URL.createObjectURL(imageBlob);
 
-    const link = document.createElement('a')
-    link.href = imageURL
-    link.download = 'mooddiary'
-    document.body.appendChild(link)
-    link.click()
-    document.body.removeChild(link)
+    const link = document.createElement('a');
+    link.href = imageURL;
+    link.download = 'mooddiary';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
 }
